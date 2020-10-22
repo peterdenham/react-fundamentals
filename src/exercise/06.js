60d457f62156b9412c49ecb5f66e828edad9fea1
Peter Denham <peter.denham@plastiq.com>
2020-10-21 20:29:25 -0400

    Revert "form basics"
    This reverts commit f89e9b7d

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,18 +5,25 @@ import React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  // 💰 Make sure to accept the `event` as an argument and call
+  // `event.preventDefault()` to prevent the default behavior of form submit
+  // events (which refreshes the page).
+  //
+  // 🐨 get the value from the username input (using whichever method
+  // you prefer from the options mentioned in the instructions)
+  // 💰 For example: event.target.elements[0].value
+  // 🐨 Call `onSubmitUsername` with the value of the input
 
-  function handleSubmit(event) {
-    const value = event.target.elements.username.value
-    event.preventDefault()
-    onSubmitUsername(value)
-  }
+  // 🐨 add the onSubmit handler to the <form> below
 
+  // 🐨 replace input's name attribute with id attribute
+  // 🐨 make sure to associate the label to the input. 
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <div>
-        <label htmlFor="username">Username:</label>
-        <input id="username" type="text" />
+        <label>Username:</label>
+        <input name="username" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
